70320feb9dfe859725d5a3a84070ea41f4a2b829
Paulus Schoutsen <balloob@gmail.com>
2021-01-30 21:38:23 +0100

    Do ZSH first, then apt install to get faster usable codespace

--- install.sh
+++ install.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-
-# APT
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get install -y zsh fzf vim
-sudo apt-get autoremove -y
-sudo apt-get clean -y
-sudo rm -rf /var/lib/apt/lists/*
 
 # Git
 ln -sf ~/dotfiles/config/.gitconfig ~
 ln -sf ~/dotfiles/config/.gitignore ~
-
-# GH CLI
-curl -s https://api.github.com/repos/cli/cli/releases/latest \
-  | jq '.assets[] | select(.name | endswith("_linux_amd64.deb")).browser_download_url' \
-  | xargs curl -O -L
-
-sudo apt-get install -y ./gh_*.deb
-rm ./gh_*.deb
 
 # ZSH
 ln -sf ~/dotfiles/config/.zshrc ~/.zshrc
@@ -27,6 +12,7 @@ chsh -s $(which zsh)
 
 # Oh My ZSH
 rm -rf ~/.oh-my-zsh
+touch ~/.z  # So it doesn't complain on very first usage
 CHSH=no RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Oh my ZSH theme
@@ -36,3 +22,18 @@ ln -sf ~/dotfiles/config/.p10k.zsh ~/.p10k.zsh
 # Oh my ZSH plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions --depth 1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting --depth 1
+
+# GH CLI
+curl -s https://api.github.com/repos/cli/cli/releases/latest \
+  | jq '.assets[] | select(.name | endswith("_linux_amd64.deb")).browser_download_url' \
+  | xargs curl -O -L
+
+sudo apt-get install -y ./gh_*.deb
+rm ./gh_*.deb
+
+# APT
+sudo apt-get update
+sudo apt-get install -y zsh fzf vim
+sudo apt-get autoremove -y
+sudo apt-get clean -y
+sudo rm -rf /var/lib/apt/lists/*
